1a3483d91bed4305e3dd64994279ae2b7152e54c
vinay250899 <25vinaykumarkb@gmail.com>
2021-06-21 16:56:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Day 1 React Example
+         Day 1 React Example for new commit
         </a>
       </header>
     </div>
